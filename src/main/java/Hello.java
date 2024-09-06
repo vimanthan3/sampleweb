3217402e9024358b2fd3da353b2070b4efb7b410
vimanthan <159103666+vimanthan3@users.noreply.github.com>
2024-09-06 16:14:30 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,9 +1,18 @@
-public class Hello {
-  
-  public static String GREETING = "Hello vimanthan!";
-  
-  public static void main(String []args) {
-    
-  }  
-}
+import io.rollout.rox.server.Rox;
+import java.util.concurrent.ExecutionException;
 
+public class main {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        // Initialize Flags container class
+        Flags flags = new Flags();
+
+        // Register the flags container under a namespace
+        Rox.register("default", flags);
+
+        // Setup connection with the feature management environment key
+        Rox.setup(af4be342-4689-40d7-7fc2-4d194dade16f).get();
+
+        // Prints the value of the boolean enableTutorial flag
+        printf("enableTutorial value is %s", flags.enableTutorial.isEnabled() ? "true" : "false");
+    }
+}
